9fa4a79a9775c2bc3217a14a3d89d330aab733e1
Lochie Ferrier <lochie@mit.edu>
2017-03-15 10:36:37 -0400

    IT WORKS YAY

---
+++ flight-comp/sim.py
@@ -0,0 +1,15 @@
+from gpkit import Model, Variable
+CDA0 = Variable("CDA0",0.004,"m^2")
+e = Variable("e",0.95,"-")
+tau = Variable("tau",0.11,"-")
+epsilon = Variable("epsilon",0.03,"-")
+W_fuse = Variable("W_fuse",2.7,"N")
+rho_foam = Variable('rho_foam',32,'kg/m^3')
+E_foam = Variable("E_foam",19.3e6,"Pa")
+c = Variable
+constraints = [
+	A = 
+
+]
+
+objective = 1/Wpay
